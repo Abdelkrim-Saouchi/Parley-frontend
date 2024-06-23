b02785a20a25c7bc40a391ee5140b65912c1271c
Abdelkrim Saouchi <krimouv1995@gmail.com>
2024-06-23 13:46:20 +0100

    Implement header

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import profileImg from "../assets/profile.jpg";
+const Header = () => {
+  return (
+    <header className="text-inter ml-[128px] flex flex-row-reverse items-center gap-20 p-4 px-16">
+      <Link to="#" className="flex items-center gap-4">
+        <div className="flex flex-col text-lg">
+          <p className="font-bold">Krimothiazine</p>
+          <p className="self-end">Online</p>
+        </div>
+        <img src={profileImg} className="size-16 rounded-full border" />
+      </Link>
+      <button>
+        <span className="icon-[hugeicons--notification-01] size-8"></span>
+      </button>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,11 @@
+import Header from "./Header";
 import SideBar from "./SideBar";
 
 const Layout = () => {
   return (
     <>
       <SideBar />
+      <Header />
     </>
   );
 };
