df7c217d5da76cca83b05c88ae556f0ac148af1e
Abdelkrim Saouchi <krimouv1995@gmail.com>
2024-06-29 11:39:59 +0100

    Reorder routes hierarchy

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,25 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import Layout from "./components/Layout.tsx";
+import HistoryPage from "./pages/HistoryPage.tsx";
+import HistoryChatWindow from "./components/HistoryChatWindow.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    children: [
+      {
+        path: "/history",
+        element: <HistoryPage />,
+        children: [
+          {
+            path: ":id",
+            element: <HistoryChatWindow />,
+          },
+        ],
+      },
+    ],
   },
 ]);
 
