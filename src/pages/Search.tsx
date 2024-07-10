92ac7aaf4dad2cc1234f06021cc17c5bc46f2d6c
Abdelkrim Saouchi <krimouv1995@gmail.com>
2024-07-10 14:10:46 +0100

    Remove pagination when results are not found

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -107,23 +107,25 @@ const Search = () => {
           })}
       </div>
 
-      <div className="mt-8 space-x-4 font-bold text-secondary">
-        <button>
-          <span className="icon-[ep--arrow-left-bold]"></span>
-        </button>
+      {results.length > 0 && (
+        <div className="mt-8 space-x-4 font-bold text-secondary">
+          <button>
+            <span className="icon-[ep--arrow-left-bold]"></span>
+          </button>
 
-        <button className="rounded-full bg-secondary px-2 text-background">
-          1
-        </button>
-        <button>2</button>
-        <button>3</button>
-        <span>...</span>
-        <button>10</button>
+          <button className="rounded-full bg-secondary px-2 text-background">
+            1
+          </button>
+          <button>2</button>
+          <button>3</button>
+          <span>...</span>
+          <button>10</button>
 
-        <button>
-          <span className="icon-[ep--arrow-right-bold]"></span>
-        </button>
-      </div>
+          <button>
+            <span className="icon-[ep--arrow-right-bold]"></span>
+          </button>
+        </div>
+      )}
     </main>
   );
 };
