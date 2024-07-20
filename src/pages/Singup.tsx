9f59d039dd3d338e50661563d126b286bf213cbc
Abdelkrim Saouchi <krimouv1995@gmail.com>
2024-07-20 12:01:15 +0100

    Implement React hook form to validation signup form

--- src/pages/Singup.tsx
+++ src/pages/Singup.tsx
@@ -2,8 +2,26 @@ import parleyLogo from "../assets/parley.svg";
 import googleLogo from "../assets/google.svg";
 import githubLogo from "../assets/github.svg";
 import signupImg from "../assets/signup-image.jpg";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+type Inputs = {
+  email: string;
+  fullName: string;
+  password: string;
+  confirmation: string;
+};
 
 const Signup = () => {
+  const form = useForm<Inputs>();
+  console.log(form);
+  const { register, formState, getValues, handleSubmit } = form;
+  const { errors } = formState;
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
   return (
     <main className="flex min-h-dvh bg-background font-inter text-lg text-primary">
       <img src={signupImg} className="hidden w-1/2 object-cover md:block" />
@@ -40,50 +58,113 @@ const Signup = () => {
             <hr className="size-0.5 w-full bg-secondary" />
           </div>
 
-          <form className="flex flex-col gap-8">
-            <div className="flex flex-col">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-8"
+          >
+            <div className="relative flex flex-col">
               <label htmlFor="email">Email</label>
               <input
                 type="email"
                 id="email"
-                name="email"
-                className="border border-secondary bg-transparent px-4 py-4 outline-accent"
+                {...register("email", {
+                  required: "Invalid email",
+                  minLength: 4,
+                  pattern: /[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]/,
+                })}
+                className={
+                  errors.email
+                    ? "border border-red-600 bg-transparent px-4 py-4 outline-accent"
+                    : "border border-secondary bg-transparent px-4 py-4 outline-accent"
+                }
               />
+              {errors.email && (
+                <span className="absolute right-0 top-0 text-red-600">
+                  Invalid email
+                </span>
+              )}
             </div>
-            <div className="flex flex-col">
+            <div className="relative flex flex-col">
               <label htmlFor="fullName">Full name</label>
               <input
                 type="fullName"
                 id="fullName"
-                name="fullName"
-                className="border border-secondary bg-transparent px-4 py-4 outline-accent"
+                {...register("fullName", {
+                  required: "Invalid Full name",
+                  minLength: 3,
+                })}
+                className={
+                  errors.fullName
+                    ? "border border-red-600 bg-transparent px-4 py-4 outline-accent"
+                    : "border border-secondary bg-transparent px-4 py-4 outline-accent"
+                }
               />
+              {errors.fullName && (
+                <span className="absolute right-0 top-0 text-red-600">
+                  Invalid full Name
+                </span>
+              )}
             </div>
-            <div className="flex flex-col">
+            <div className="relative flex flex-col">
               <label htmlFor="password">Password</label>
               <input
                 type="password"
                 id="password"
-                name="password"
-                className="border border-secondary bg-transparent px-4 py-4 outline-accent"
+                {...register("password", {
+                  required: "Invalid password",
+                  minLength: 5,
+                })}
+                className={
+                  errors.password
+                    ? "border border-red-600 bg-transparent px-4 py-4 outline-accent"
+                    : "border border-secondary bg-transparent px-4 py-4 outline-accent"
+                }
               />
+              {errors.password && (
+                <span className="absolute right-0 top-0 text-red-600">
+                  Invalid password
+                </span>
+              )}
             </div>
-            <div className="flex flex-col">
+            <div className="relative flex flex-col">
               <label htmlFor="confirmation">Confirmation</label>
               <input
-                type="confirmation"
+                type="password"
                 id="confirmation"
-                name="confirmation"
-                className="border border-secondary bg-transparent px-4 py-4 outline-accent"
+                {...register("confirmation", {
+                  validate: (fieldValue) => {
+                    const pswd = getValues("password");
+
+                    return pswd === fieldValue || "Password mismatch";
+                  },
+                })}
+                className={
+                  errors.confirmation
+                    ? "border border-red-600 bg-transparent px-4 py-4 outline-accent"
+                    : "border border-secondary bg-transparent px-4 py-4 outline-accent"
+                }
               />
+              {errors.confirmation && (
+                <span className="absolute right-0 top-0 text-red-600">
+                  Passwords mismatch
+                </span>
+              )}
             </div>
-            <button className="bg-accent py-4 font-semibold text-background transition-all hover:scale-105">
+            <button
+              type="submit"
+              className="bg-accent py-4 font-semibold text-background transition-all hover:scale-105"
+            >
               SIGN UP
             </button>
           </form>
           <div className="mt-4 flex justify-between">
             <p>Do you have an account?</p>
-            <a className="cursor-pointer text-accent hover:underline">Log in</a>
+            <Link
+              to="/login"
+              className="cursor-pointer text-accent hover:underline"
+            >
+              Log in
+            </Link>
           </div>
         </div>
       </div>
